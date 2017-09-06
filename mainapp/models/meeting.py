e25114185227d2d22d19f5e8dc1190b7591b103d
konstin <konstin@mailbox.org>
2017-09-06 17:21:09 +0200

    File attachments for Meeting

--- mainapp/models/meeting.py
+++ mainapp/models/meeting.py
@@ -2,7 +2,9 @@ from django.db import models
 
 from .committee import Committee
 from .default_fields import DefaultFields
+from .file import File
 from .location import Location
+from .person import Person
 
 
 class Meeting(DefaultFields):
@@ -12,4 +14,11 @@ class Meeting(DefaultFields):
     end = models.DateTimeField()
     locations = models.ForeignKey(Location, null=True, blank=True)
     # There are cases where mutliple committes have a joined official meeting
-    committees = models.ManyToManyField(Committee)
+    committees = models.ManyToManyField(Committee, blank=True)
+    # Only applicable when there are participants without an organization
+    persons = models.ManyToManyField(Person, blank=True)
+    invitation = models.ForeignKey(File, null=True, blank=True)
+    resultsProtocol = models.ForeignKey(File, null=True, blank=True)
+    verbatim_protocol = models.ForeignKey(File, null=True, blank=True)
+    # Sometimes there are additional files atttached to a meeting
+    auxiliary_files = models.ManyToManyField(File, blank=True)
