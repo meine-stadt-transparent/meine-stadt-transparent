29fb0b9e0a8100e774f10b68fb4ca52cdde55d2d
konstin <konstin@mailbox.org>
2017-09-06 18:06:03 +0200

    Mute irrelevant warning

--- opensourceris/settings.py
+++ opensourceris/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import warnings
+
+# Mute an irrelevant warning
+warnings.filterwarnings("ignore", message="`django-leaflet` is not available.")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
