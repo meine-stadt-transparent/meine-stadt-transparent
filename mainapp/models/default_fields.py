0893c85642c8a923d7fab7e05fb3a3829cc83bca
konstin <konstin@mailbox.org>
2017-10-04 14:08:01 +0200

    Make oparl_id unique

--- mainapp/models/default_fields.py
+++ mainapp/models/default_fields.py
@@ -5,7 +5,7 @@ class DefaultFields(models.Model):
     """
     These fields are mainly inspired and required by oparl
     """
-    oparl_id = models.CharField(max_length=2000, null=True, blank=True)
+    oparl_id = models.CharField(max_length=255, null=True, blank=True, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
