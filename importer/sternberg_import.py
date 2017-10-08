64fd40732c3de09cdb19b8f49be92a0f52a954e7
Tobias Hößl <tobias@hoessl.eu>
2017-10-08 13:52:11 +0200

    Try fixing Jülich

--- importer/sternberg_import.py
+++ importer/sternberg_import.py
@@ -11,12 +11,29 @@ class SternbergImport(OParlImporter):
         if not response.get_success() or "/body" not in url:
             return response
 
-        oparl_list = json.loads(response.get_resolved_data())
-        if "data" in oparl_list:
-            for oparl_object in oparl_list["data"]:
-                if "location" in oparl_object.keys() and type(oparl_object["location"]) != str:
-                    oparl_object["location"]["type"] = "https://schema.oparl.org/1.0/Location"
-        response.set_resolved_data(json.dumps(oparl_list))
+        if "/body" in url:
+            oparl_list = json.loads(response.get_resolved_data())
+
+            # Add missing "type"-attributes in body-lists
+            if "data" in oparl_list:
+                for oparl_object in oparl_list["data"]:
+                    if "location" in oparl_object.keys() and type(oparl_object["location"]) != str:
+                        oparl_object["location"]["type"] = "https://schema.oparl.org/1.0/Location"
+
+            # Add missing "type"-attributes in single bodies
+            if "location" in oparl_list.keys() and type(oparl_list["location"]) != str:
+                oparl_list["location"]["type"] = "https://schema.oparl.org/1.0/Location"
+
+            response.set_resolved_data(json.dumps(oparl_list))
+
+        if "/membership" in url:
+            oparl_list = json.loads(response.get_resolved_data())
+
+            # If an array is resturned instead of an object, we just skip all list entries except for the last one
+            if isinstance(oparl_list, list):
+                oparl_list = oparl_list[len(oparl_list) - 1]
+
+            response.set_resolved_data(json.dumps(oparl_list))
 
         return response
 
