9189649d21f12d5a2d7f2b3b4b71cd8eb25b1deb
konstin <konstin@mailbox.org>
2017-10-09 14:27:27 +0200

    static files

--- meine_stadt_transparent/settings.py
+++ meine_stadt_transparent/settings.py
@@ -208,6 +208,7 @@ TIME_FORMAT = "%H:%M"
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mainapp/assets'),
