d11da06ee744cfb51793ddbaab43aa4f24143775
Tobias Hößl <tobias@hoessl.eu>
2017-10-27 14:50:11 +0200

    Prevent access to deleted objects

--- mainapp/models/default_fields.py
+++ mainapp/models/default_fields.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class SoftDeleteModelManager(models.Manager):
+    def get_queryset(self):
+        queryset = models.query.QuerySet(self.model, using=self._db)
+        return queryset.filter(deleted=0)
+
+
+class SoftDeleteModelManagerWithDeleted(models.Manager):
+    def get_queryset(self):
+        return models.query.QuerySet(self.model, using=self._db)
+
+
 class DefaultFields(models.Model):
     """
     These fields are mainly inspired and required by oparl
@@ -9,6 +20,9 @@ class DefaultFields(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteModelManager()
+    objects_with_deleted = SoftDeleteModelManagerWithDeleted()
 
     @classmethod
     def by_oparl_id(cls, oparl_id):
---
+++ mainapp/tests/test_document_access.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, override_settings
+
+from mainapp.models import Paper, File
+
+
+@override_settings(ELASTICSEARCH_DSL_AUTOSYNC=False, ELASTICSEARCH_DSL_AUTO_REFRESH=False)
+class TestDocumentAccess(TestCase):
+    fixtures = ['initdata']
+
+    def test_delete_document(self):
+        paper = Paper.objects.get(pk=1)
+
+        file = File.objects.get(pk=2)
+        file_papers = file.paper_set.all()
+        self.assertEqual(1, len(file_papers))
+
+        # Now we delete the paper
+        paper.deleted = True
+        paper.save()
+
+        file_papers = file.paper_set.all()
+        self.assertEqual(0, len(file_papers))
+
+        with self.assertRaises(Paper.DoesNotExist):
+            Paper.objects.get(pk=1)
+
+        # Now we restore it
+        deleted_paper = Paper.objects_with_deleted.get(pk=1)
+        deleted_paper.deleted = False
+        deleted_paper.save()
+
+        paper = Paper.objects.get(pk=1)
+        self.assertEqual(1, paper.id)
+        file_papers = file.paper_set.all()
+        self.assertEqual(1, len(file_papers))
