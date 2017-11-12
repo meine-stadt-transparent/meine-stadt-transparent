f1b06f616eba9f5776984c2c7097a64d0f792133
Tobias Hößl <tobias@hoessl.eu>
2017-11-12 22:12:40 +0100

    Test cases for person list

--- mainapp/tests/live/test_persons.py
+++ mainapp/tests/live/test_persons.py
@@ -6,7 +6,6 @@ from splinter import Browser
 
 class PersonsTest(StaticLiveServerTestCase):
     fixtures = ['initdata.json']
-    selenium = None
     browser = None
 
     @classmethod
@@ -16,17 +15,59 @@ class PersonsTest(StaticLiveServerTestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # cls.selenium.quit()
         cls.browser.quit()
         super(PersonsTest, cls).tearDownClass()
 
+    def _get_pos_by_name(self, name):
+        js = 'window.jQuery(".persons-list").data("get-item-pos-by-name")("' + name + '");'
+        pos = self.browser.evaluate_script(js)
+        return pos
+
     def test_filter(self):
         self.browser.visit('%s%s' % (self.live_server_url, '/persons/'))
-        self.assertTrue(self.browser.find_by_text('Bob Birch').first.visible)
-        self.assertTrue(self.browser.find_by_text('William Conway').first.visible)
+
+        # Both are visible
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertIsNotNone(pos_peter)
+        self.assertIsNotNone(pos_hector)
 
         # Filter for democrats
-        self.browser.find_by_css('.filter-parliamentary-groups .btn-secondary').first.click()
+        self.browser.find_by_css('.filter-parliamentary-groups .parliamentary-group-1').first.click()
         time.sleep(1)
-        self.assertTrue(self.browser.find_by_text('Bob Birch').first.visible)
-        self.assertFalse(self.browser.find_by_text('William Conway').first.visible)
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertIsNotNone(pos_peter)
+        self.assertIsNone(pos_hector)
+
+        # Filter for republicans
+        self.browser.find_by_css('.filter-parliamentary-groups .parliamentary-group-2').first.click()
+        time.sleep(1)
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertIsNone(pos_peter)
+        self.assertIsNotNone(pos_hector)
+
+    def test_sort(self):
+        self.browser.visit('%s%s' % (self.live_server_url, '/persons/'))
+
+        # Default sorting: by name
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertLess(pos_hector, pos_peter)
+
+        # Switch to sorting by party
+        self.browser.find_by_css('#btnSortDropdown').first.click()
+        self.browser.find_by_css('.sort-selector a[data-sort="group"]').first.click()
+        time.sleep(1)
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertLess(pos_peter, pos_hector)
+
+        # Switch back to sorting by name
+        self.browser.find_by_css('#btnSortDropdown').first.click()
+        self.browser.find_by_css('.sort-selector a[data-sort="name"]').first.click()
+        time.sleep(1)
+        pos_peter = self._get_pos_by_name('Peter Russo')
+        pos_hector = self._get_pos_by_name('Hector Mendoza')
+        self.assertLess(pos_hector, pos_peter)
