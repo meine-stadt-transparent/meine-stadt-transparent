e4c17e65ae13571c473c4a1020d021635a1ef6d4
Tobias Hößl <tobias@hoessl.eu>
2017-11-26 15:40:03 +0100

    More login test cases

--- mainapp/tests/live/chromedriver_test_case.py
+++ mainapp/tests/live/chromedriver_test_case.py
@@ -1,18 +1,67 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.options import Options
+from django.test import modify_settings
 from splinter import Browser
 
 chromedriver_path = "node_modules/.bin/chromedriver"
 
 
+"""
+Specifics of ChromeDriverTestCase:
+- Chrome Headless is used
+- English is used for the UI
+- CSRF-checks are disabled, as referrer-checking seems to be problematic, as the HTTPS-header seems to be always set
+"""
+
+
+@modify_settings(MIDDLEWARE={
+    'remove': ['django.middleware.csrf.CsrfViewMiddleware'],
+})
 class ChromeDriverTestCase(StaticLiveServerTestCase):
     browser = None
 
     @classmethod
     def setUpClass(cls):
-        cls.browser = Browser('chrome', headless=True, executable_path=chromedriver_path)
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'en_US'})
+        cls.browser = Browser('chrome', headless=True, executable_path="node_modules/.bin/chromedriver", options=options)
         super(ChromeDriverTestCase, cls).setUpClass()
 
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
         super(ChromeDriverTestCase, cls).tearDownClass()
+
+    """
+    Helper functions for a more convenient test syntax
+    """
+
+    def assertTextIsPresent(self, text):
+        self.assertTrue(self.browser.is_text_present(text))
+
+    def assertTextIsNotPresent(self, text):
+        self.assertFalse(self.browser.is_text_present(text))
+
+    def assertElementDoesExists(self, css_selector):
+        self.assertTrue(self.browser.is_element_present_by_css(css_selector))
+
+    def assertElementDoesNotExists(self, css_selector):
+        self.assertFalse(self.browser.is_element_present_by_css(css_selector))
+
+    """
+    Functions for behaviors used by several test cases
+    """
+
+    def logout(self):
+        self.browser.find_by_css('#main-menu-content .my-account-link').click()
+        self.browser.find_by_css('.logout-form button').click()
+        self.assertTextIsPresent('You have signed out.')
+        self.assertElementDoesExists('#main-menu-content .login-link')
+        self.assertElementDoesNotExists('#main-menu-content .my-account-link')
+
+    def login(self, username, password):
+        self.browser.find_by_css('#main-menu-content .login-link').click()
+        self.browser.fill('login', username)
+        self.browser.fill('password', password)
+        self.browser.find_by_css('form.login button').click()
+        self.assertTextIsPresent('Successfully signed in as ' + username)
--- mainapp/tests/live/test_login.py
+++ mainapp/tests/live/test_login.py
@@ -1,38 +1,57 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.chrome.options import Options
-from django.test import modify_settings
-from splinter import Browser
+from mainapp.tests.live.chromedriver_test_case import ChromeDriverTestCase
 
 
-@modify_settings(MIDDLEWARE={
-    'remove': ['django.middleware.csrf.CsrfViewMiddleware'],
-})  # There seems to be a strange but where HTTPS=on is always set, making all CSRF-checks fail
-class PersonsTest(StaticLiveServerTestCase):
+class PersonsTest(ChromeDriverTestCase):
     fixtures = ['initdata.json']
-    browser = None
 
-    @classmethod
-    def setUpClass(cls):
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'en_US'})
-        cls.browser = Browser('chrome', headless=True, executable_path="node_modules/.bin/chromedriver", options=options)
-        super(PersonsTest, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.browser.quit()
-        super(PersonsTest, cls).tearDownClass()
-
-    def test_register(self):
-        temp_password = '8I$KJ37Kdk'
+    def test_register_change_password(self):
+        temp_password = '8I$KJ37Kdk'  # Only used for this test case, so no harm done in committing this password ☺️
         temp_email = 'test@example.org'
 
+        # Check if I can register a new account
         browser = self.browser
         browser.visit('%s%s' % (self.live_server_url, '/'))
+        self.assertElementDoesNotExists('.my-account-link')
         browser.find_by_css('#main-menu-content .login-link').click()
         browser.find_by_css('.signup-link').click()
         browser.fill('email', temp_email)
         browser.fill('password1', temp_password)
         browser.find_by_css('#signup_form .save-row button').click()
-        if not browser.is_text_present('Confirmation e-mail sent to ' + temp_email):
-            self.fail('Confirmation message is not present')
+        self.assertTextIsPresent('Confirmation e-mail sent to ' + temp_email)
+        self.assertElementDoesExists('.my-account-link')
+
+        # Check if I can login using this new account
+        self.logout()
+        self.login(temp_email, temp_password)
+
+        # Check if I can change my password
+        temp_password2 = 'sdir23744!ä'
+        browser.find_by_css('#main-menu-content .my-account-link').click()
+        browser.find_by_css('.change-password-link').click()
+        # First, try it with an invalid password
+        browser.fill('oldpassword', 'wrongpassword')
+        browser.fill('password1', temp_password2)
+        browser.fill('password2', temp_password2)
+        browser.find_by_css('form.password_change .save-row button').click()
+        self.assertTextIsPresent('Please type your current password')
+        self.assertTextIsNotPresent('Password successfully changed')
+        # Now change it for real
+        browser.fill('oldpassword', temp_password)
+        browser.fill('password1', temp_password2)
+        browser.fill('password2', temp_password2)
+        browser.find_by_css('form.password_change .save-row button').click()
+        self.assertTextIsNotPresent('Please type your current password')
+        self.assertTextIsPresent('Password successfully changed')
+
+        # Check that I cannot log in with the old, but therefore with the new password
+        self.logout()
+        self.browser.find_by_css('#main-menu-content .login-link').click()
+        self.browser.fill('login', temp_email)
+        self.browser.fill('password', temp_password)
+        self.browser.find_by_css('form.login button').click()
+        self.assertTextIsNotPresent('Successfully signed in')
+        self.assertTextIsPresent('The e-mail address and/or password you specified are not correct')
+        self.browser.fill('login', temp_email)
+        self.browser.fill('password', temp_password2)
+        self.browser.find_by_css('form.login button').click()
+        self.assertTextIsPresent('Successfully signed in')
