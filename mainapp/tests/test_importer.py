d7467eac712cd6eb889ba8ba1c9377e1c74d4a1a
konstin <konstin@mailbox.org>
2017-11-27 14:59:53 +0100

    Extend importer test
    Even it doesn't run on travis I should have done this much earlier

--- mainapp/tests/test_importer.py
+++ mainapp/tests/test_importer.py
@@ -6,6 +6,7 @@ import shutil
 from unittest import skipIf
 
 from django.test import TestCase
+from django.utils import timezone
 
 from mainapp.models import Body, LegislativeTerm, Organization, Person, OrganizationMembership, Meeting, AgendaItem, \
     Paper, Consultation, Location, File
@@ -19,16 +20,20 @@ if not gi_not_available:
 
 @skipIf(gi_not_available, "gi is not available")
 class TestImporter(TestCase):
-    dummy_data = "testdata/oparl"
-    fake_cache = "testdata/fake_cache"
-    entrypoint = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.new_timestamp = None
+        self.delete = False
+        self.dummy_data = "testdata/oparl"
+        self.fake_cache = "testdata/fake_cache"
+        self.entrypoint = None
 
     def sha1(self, data):
         return hashlib.sha1(data.encode("utf-8")).hexdigest()
 
     def load(self, name):
         with open(os.path.join(self.dummy_data, name)) as f:
-            return json.load(f)
+            return self.manipulate(json.load(f), self.new_timestamp)
 
     def dump(self, name, obj):
         with open(os.path.join(self.fake_cache, self.sha1(self.entrypoint), self.sha1(name)), 'w') as f:
@@ -37,7 +42,22 @@ class TestImporter(TestCase):
     def external_list(self, obj):
         return {"data": [obj], "links": {}, "pagination": {}}
 
-    def setUp(self):
+    def manipulate(self, obj, new_value: str):
+        if self.delete:
+            obj["deleted"] = True
+
+        for key, value in obj.items():
+            if key == "modified" or key == "created":
+                obj[key] = new_value
+            if isinstance(value, list):
+                for i in value:
+                    if isinstance(i, dict):
+                        self.manipulate(i, new_value)
+            if isinstance(value, dict):
+                self.manipulate(value, new_value)
+        return obj
+
+    def create_fake_cache(self):
         """ Fakes an oparl server by a creating a prefilled cache. """
         # Load the entrypoint first, we need the id for the remaining setup
         system = self.load("System.json")
@@ -76,6 +96,47 @@ class TestImporter(TestCase):
         self.dump(membership["id"], membership)
 
     def test_importer(self):
+        self.new_timestamp = "2000-01-01T00:00:00+01:00"
+        self.create_fake_cache()
+        options = self.build_options()
+        importer = OParlImport(options)
+        importer.run_singlethread()
+
+        now = timezone.now()
+
+        tables = [Body, LegislativeTerm, Organization, Person, OrganizationMembership, Meeting, AgendaItem, Paper,
+                  Consultation, Location]
+
+        for table in tables:
+            self.assertEqual(table.objects.count(), 1)
+            self.assertLess(table.objects.first().modified, now)
+
+        self.assertEqual(File.objects.count(), 2)
+
+        self.new_timestamp = "2020-01-01T00:00:00+01:00"
+        self.create_fake_cache()
+        importer = OParlImport(options)
+        importer.run_singlethread()
+
+        for table in tables:
+            self.assertEqual(table.objects.count(), 1)
+            self.assertGreater(table.objects.first().modified, now)
+
+        self.assertEqual(File.objects.count(), 2)
+
+        self.new_timestamp = "2030-01-01T00:00:00+01:00"
+        self.delete = True
+        self.create_fake_cache()
+        importer = OParlImport(options)
+        importer.run_singlethread()
+
+        # FIXME: Deletion isn't working properly. See #15
+        tables = [Body, Organization, Person, Meeting, Paper]
+
+        for table in tables:
+            self.assertEqual(table.objects.count(), 0)
+
+    def build_options(self):
         options = default_options.copy()
         options["cachefolder"] = self.fake_cache
         options["storagefolder"] = "/tmp/meine_stadt_transparent/storagefolder"
@@ -83,16 +144,7 @@ class TestImporter(TestCase):
             shutil.rmtree(options["storagefolder"])
         options["entrypoint"] = self.entrypoint
         options["batchsize"] = 1
-        importer = OParlImport(options)
-        importer.run_singlethread()
-
-        tables = [Body, LegislativeTerm, Organization, Person, OrganizationMembership, Meeting, AgendaItem, Paper,
-                  Consultation, Location]
-
-        for table in tables:
-            self.assertEqual(table.objects.count(), 1)
-
-        self.assertEqual(File.objects.count(), 2)
+        return options
 
     def tearDown(self):
         if os.path.isdir(self.fake_cache):
