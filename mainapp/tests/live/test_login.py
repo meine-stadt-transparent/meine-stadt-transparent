1fd9c90feba43ea7c6be3e707fab960caf1a8efa
konstin <konstin@mailbox.org>
2017-11-27 15:40:05 +0100

    Refactor test case

--- mainapp/tests/live/test_login.py
+++ mainapp/tests/live/test_login.py
@@ -1,57 +1,62 @@
 from mainapp.tests.live.chromedriver_test_case import ChromeDriverTestCase
 
 
-class PersonsTest(ChromeDriverTestCase):
+class AccountTest(ChromeDriverTestCase):
     fixtures = ['initdata.json']
+    password1 = '8I$KJ37Kdk'
+    email = 'test@example.org'
+    password2 = 'sdir23744!ä'
 
-    def test_register_change_password(self):
-        temp_password = '8I$KJ37Kdk'  # Only used for this test case, so no harm done in committing this password ☺️
-        temp_email = 'test@example.org'
-
-        # Check if I can register a new account
-        browser = self.browser
-        browser.visit('%s%s' % (self.live_server_url, '/'))
+    def check_register_new_account(self, email, password):
+        self.browser.visit('%s%s' % (self.live_server_url, '/'))
         self.assertElementDoesNotExists('.my-account-link')
-        browser.find_by_css('#main-menu-content .login-link').click()
-        browser.find_by_css('.signup-link').click()
-        browser.fill('email', temp_email)
-        browser.fill('password1', temp_password)
-        browser.find_by_css('#signup_form .save-row button').click()
-        self.assertTextIsPresent('Confirmation e-mail sent to ' + temp_email)
+        self.browser.find_by_css('#main-menu-content .login-link').click()
+        self.browser.find_by_css('.signup-link').click()
+        self.browser.fill('email', email)
+        self.browser.fill('password1', password)
+        self.browser.find_by_css('#signup_form .save-row button').click()
+        self.assertTextIsPresent('Confirmation e-mail sent to ' + email)
         self.assertElementDoesExists('.my-account-link')
 
-        # Check if I can login using this new account
-        self.logout()
-        self.login(temp_email, temp_password)
-
-        # Check if I can change my password
-        temp_password2 = 'sdir23744!ä'
-        browser.find_by_css('#main-menu-content .my-account-link').click()
-        browser.find_by_css('.change-password-link').click()
+    def check_change_password(self, password):
+        self.browser.find_by_css('#main-menu-content .my-account-link').click()
+        self.browser.find_by_css('.change-password-link').click()
         # First, try it with an invalid password
-        browser.fill('oldpassword', 'wrongpassword')
-        browser.fill('password1', temp_password2)
-        browser.fill('password2', temp_password2)
-        browser.find_by_css('form.password_change .save-row button').click()
+        self.browser.fill('oldpassword', 'wrongpassword')
+        self.browser.fill('password1', self.password2)
+        self.browser.fill('password2', self.password2)
+        self.browser.find_by_css('form.password_change .save-row button').click()
         self.assertTextIsPresent('Please type your current password')
         self.assertTextIsNotPresent('Password successfully changed')
         # Now change it for real
-        browser.fill('oldpassword', temp_password)
-        browser.fill('password1', temp_password2)
-        browser.fill('password2', temp_password2)
-        browser.find_by_css('form.password_change .save-row button').click()
-        self.assertTextIsNotPresent('Please type your current password')
+        self.browser.fill('oldpassword', password)
+        self.browser.fill('password1', self.password2)
+        self.browser.fill('password2', self.password2)
+        self.browser.find_by_css('form.password_change .save-row button').click()
         self.assertTextIsPresent('Password successfully changed')
+        self.assertTextIsNotPresent('Please type your current password')
+        return self.password2
 
-        # Check that I cannot log in with the old, but therefore with the new password
+    def check_password_invalidated(self, email, password, password2):
+        """ Check that I cannot log in with the old, but therefore with the new password. """
         self.logout()
         self.browser.find_by_css('#main-menu-content .login-link').click()
-        self.browser.fill('login', temp_email)
-        self.browser.fill('password', temp_password)
+        self.browser.fill('login', email)
+        self.browser.fill('password', password)
         self.browser.find_by_css('form.login button').click()
         self.assertTextIsNotPresent('Successfully signed in')
         self.assertTextIsPresent('The e-mail address and/or password you specified are not correct')
-        self.browser.fill('login', temp_email)
-        self.browser.fill('password', temp_password2)
+        self.browser.fill('login', email)
+        self.browser.fill('password', password2)
         self.browser.find_by_css('form.login button').click()
         self.assertTextIsPresent('Successfully signed in')
+
+    def test_register_change_password(self):
+        self.check_register_new_account(self.email, self.password1)
+
+        # Check if I can login using this new account
+        self.logout()
+        self.login(self.email, self.password1)
+
+        password2 = self.check_change_password(self.password1)
+        self.check_password_invalidated(self.email, self.password1, password2)
