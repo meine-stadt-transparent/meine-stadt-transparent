96353c2615b0af8dc4ac6faf285705796ca20fbf
Tobias Hößl <tobias@hoessl.eu>
2017-12-17 20:57:03 +0100

    Fix test case

--- mainapp/tests/test_searchtools.py
+++ mainapp/tests/test_searchtools.py
@@ -3,6 +3,11 @@ from django.test import TestCase
 from mainapp.functions.search_tools import search_string_to_params, params_to_query, params_to_search_string
 
 expected_params = {
+    'sort': [
+        {
+            'modified': {"order": "desc"}
+        }
+    ],
     'query': {
         'bool': {
             'filter': [
@@ -39,19 +44,20 @@ expected_params = {
 
 
 class TestSearchtools(TestCase):
-    params = {"document-type": "file,committee", "radius": "50", "searchterm": "word radius anotherword"}
+    params = {"document-type": "file,committee", "radius": "50", "searchterm": "word radius anotherword",
+              "sort": "date_newest"}
 
     def test_search_string_to_params(self):
-        instring = search_string_to_params("document-type:file,committee word  radius radius:50 anotherword")
+        instring = search_string_to_params(
+            "document-type:file,committee word  radius radius:50 sort:date_newest anotherword")
         self.assertEqual(instring, self.params)
 
     def test_params_to_query(self):
         options, query, errors = params_to_query(self.params)
         self.assertEqual(errors, [])
-        print(query.to_dict())
         self.assertEqual(query.to_dict(), expected_params)
 
     def test_params_to_search_string(self):
-        expected = "document-type:file,committee radius:50 word radius anotherword"
+        expected = "document-type:file,committee radius:50 sort:date_newest word radius anotherword"
         search_string = params_to_search_string(self.params)
         self.assertEqual(search_string, expected)
