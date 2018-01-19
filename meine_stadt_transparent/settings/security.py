48ca2d08b873f6800b4bf925d75968da7014e40d
konstin <konstin@mailbox.org>
2018-01-19 15:16:06 +0100

    Pass django's security check

--- meine_stadt_transparent/settings/security.py
+++ meine_stadt_transparent/settings/security.py
@@ -1,5 +1,12 @@
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 365 * 24 * 60 * 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 CSP_IMG_SRC = ("'self'", "data:", "api.tiles.mapbox.com", "api.mapbox.com")
 # These are covered by default-src, but we need to define them explicitly for extending them later
