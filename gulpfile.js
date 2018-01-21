6a45d7f236267d8a63c618cd0247cb898d62d031
konstin <konstin@mailbox.org>
2018-01-21 19:44:12 +0100

    Add browsersync via gulp as dev tool
    Works by running `gulp`, though CSP needs to be disabled as browser-sync does js magic

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// https://dezoito.github.io/2016/01/06/django-automate-browsersync.html
+// Extremly useful when experimenting with templates
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('default', function() {
+    browserSync.init({
+        proxy: "localhost:8000"
+    });
+    gulp.watch(['./**/*.html'], reload);
+});
+
