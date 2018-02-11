39ca32a4bfbedeb0be0f34d0fbad2e02c3239d6c
konstin <konstin@mailbox.org>
2018-02-11 17:58:46 +0100

    Enhance importlist

--- mainapp/management/commands/importlist.py
+++ mainapp/management/commands/importlist.py
@@ -8,19 +8,28 @@ class Command(ImportOParlCommand):
     def add_arguments(self, parser, add_entrypoint=True):
         super().add_arguments(parser, add_entrypoint)
         parser.add_argument('list', choices=["paper", "person", "organization", "meeting"])
+        parser.add_argument('--cutoff', type=int)
+
+    def get_with_cutoff(self, fn, cutoff):
+        if cutoff:
+            return lambda: fn()[:cutoff]
+        else:
+            return lambda: fn()
 
     def handle(self, *args, **options):
         importer = get_importer(options)(options)
+        cutoff = options["cutoff"]
 
         bodies = importer.get_bodies()
         for body in bodies:
             if options["list"] == "paper":
-                importer.list_batched(body.get_paper, importer.paper)
+                importer.list_batched(self.get_with_cutoff(body.get_paper, cutoff), importer.paper)
             elif options["list"] == "person":
-                importer.list_batched(body.get_person, importer.person)
+                importer.list_batched(self.get_with_cutoff(body.get_person, cutoff), importer.person)
             elif options["list"] == "organization":
-                importer.list_batched(body.get_organization, importer.organization)
+                importer.list_batched(self.get_with_cutoff(body.get_organization, cutoff), importer.organization)
             elif options["list"] == "meeting":
-                importer.list_batched(body.get_meeting, importer.meeting)
+                importer.list_batched(self.get_with_cutoff(body.get_meeting, cutoff), importer.meeting)
             else:
                 raise ValueError("Invalid list " + options["list"])
+            importer.add_missing_associations()
