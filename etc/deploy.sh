f2ea3c4bd8902c24fd8d6c2cff3b7d3b1e8abc72
konstin <konstin@mailbox.org>
2018-12-03 18:08:02 +0100

    Add demo site deploy script to git

---
+++ etc/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+git fetch
+git reset --hard origin/master
+poetry install --no-dev
+poetry run python ./manage.py migrate
+poetry run python ./manage.py compilemessages
+npm install
+npm run build:prod
+poetry run python ./manage.py collectstatic --noinput
+ps aux | grep gunicorn | grep meine | awk '{ print $2 }' | xargs kill -HUP
+echo "Deployment finished"
