785c5c20028542b0672c91a9cea24802114aa844
konstin <konstin@mailbox.org>
2018-12-05 13:55:34 +0100

    Workaround npm weirdness
    https://stackoverflow.com/questions/45022048/why-does-npm-install-rewrite-package-lock-json

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -7,7 +7,7 @@ git reset --hard origin/master
 poetry install --no-dev
 poetry run python ./manage.py migrate
 poetry run python ./manage.py compilemessages
-npm install
+npm ci
 npm run build:prod
 poetry run python ./manage.py collectstatic --noinput
 ps aux | grep gunicorn | grep meine | awk '{ print $2 }' | xargs kill -HUP
