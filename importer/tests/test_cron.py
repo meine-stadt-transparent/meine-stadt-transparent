af7451991ad8562e090c8e282334d36a1b269238
konstin <konstin@mailbox.org>
2019-02-21 10:34:25 +0100

    More mocking

--- importer/tests/test_cron.py
+++ importer/tests/test_cron.py
@@ -4,6 +4,7 @@
 # In[]
 
 from django.core.management import call_command
+from django.utils import timezone
 
 from importer.models import ExternalList
 from importer.tests.utils import MockLoader, old_date, make_system, make_body, make_list, make_file, make_paper
@@ -12,7 +13,7 @@ system = make_system()
 body = make_body()
 
 # In[]
-# 1. Load a fixture with two papers and two users, with one user beeing subscribed to one file, and the external lists loaded
+# 1. Load a fixture with two papers and two users, with one user beeing subscribed to one paper, and the external lists loaded
 
 ExternalList(url=system["body"], last_update=old_date).save()
 ExternalList(url=body["person"], last_update=old_date).save()
@@ -45,10 +46,18 @@ call_command('cron')
 
 # 4. Mock an extern list with changes to both paper
 
-files1 = [make_file(0), make_file(1)]
-files2 = [make_file(2), make_file(3)]
+new_date = timezone.now().astimezone().replace(microsecond=0)
 
-loader.api_data[body["paper"]] = make_list([make_paper(files1, paper_id=0), make_paper(files2, paper_id=1)])
+file1 = make_file(1)
+file2 = make_file(2)
+file2["modified"] = new_date
+file1["modified"] = new_date
+paper1 = make_paper([file1], paper_id=1)
+paper1["modified"] = new_date
+paper2 = make_paper([file2], paper_id=2)
+paper2["modified"] = new_date
+
+loader.api_data[body["paper"]] = make_list([paper1, paper2])
 
 # In[]
 
