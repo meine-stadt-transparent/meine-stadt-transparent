51adb8b5b63a11d65e49cb829f2aafa5a99073cd
konstin <konstin@mailbox.org>
2019-04-05 09:42:19 +0200

    Merge migrations

--- mainapp/migrations/0022_auto_20190404_1605.py
+++ mainapp/migrations/0022_auto_20190404_1605.py
@@ -12,11 +12,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterField(
-            model_name='historicalpaper',
-            name='change_request_of',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='mainapp.Paper'),
-        ),
-        migrations.AlterField(
             model_name='historicalperson',
             name='family_name',
             field=models.CharField(max_length=100),
