2d6bc6866013e0af17c960d8a516d03cc6263f24
konstin <konstin@mailbox.org>
2019-04-26 10:13:58 +0200

    Register deploys with sentry

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -4,6 +4,13 @@ set -xe
 
 git fetch
 git reset --hard origin/master
+
+VERSION="meine-stadt-transparent@$(git rev-parse HEAD)"
+export SENTRY_ORG=konstin
+
+sentry-cli releases new -p meine-stadt-transparent-de "$VERSION"
+sentry-cli releases set-commits "$VERSION" --auto
+
 poetry install --no-dev
 poetry run python ./manage.py migrate
 poetry run python ./manage.py compilemessages
@@ -12,3 +19,6 @@ npm run build:prod
 poetry run python ./manage.py collectstatic --noinput
 ps aux | grep gunicorn | grep meine | awk '{ print $2 }' | xargs kill -HUP
 echo "Deployment finished"
+
+sentry-cli releases deploys "$VERSION" new -e staging
+sentry-cli releases finalize "$VERSION"
