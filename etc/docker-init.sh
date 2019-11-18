6d33b62e497f1947d530e94c9c1424bff76f06a2
ubergesundheit <ubergesundheit@googlemail.com>
2019-11-18 17:22:22 +0100

    WIP container image optimization

--- etc/docker-init.sh
+++ etc/docker-init.sh
@@ -4,15 +4,15 @@
 set -e
 
 # Generate all static files and clean up all node stuff
-npm ci --dev
-npm run build:prod
-npm run build:email
+#npm ci --dev
+#npm run build:prod
+#npm run build:email
 cp etc/template.env .env
 mkdir /app/log
 poetry run python manage.py compilemessages
 poetry run python manage.py collectstatic --noinput
 rm .env
-rm -rf node_modules # We need them until after collectstatic for pdfjs-dist
+#rm -rf node_modules # We need them until after collectstatic for pdfjs-dist
 npm install osmtogeojson  # We call that tool from python
 
 chown -R www-data:www-data /app
