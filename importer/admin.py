454e1eac472db2b4fa2e4badcbc35aec438dbc93
konstin <konstin@mailbox.org>
2020-03-06 14:04:30 +0100

    Add importer models to admin

--- importer/admin.py
+++ importer/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from importer.models import CachedObject, ExternalList
+
+admin.site.register(CachedObject)
+admin.site.register(ExternalList)
