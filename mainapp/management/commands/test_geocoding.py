7b83ea11e5c6214f6e553edfbf8b3fc095837314
konstin <konstin@mailbox.org>
2020-03-23 14:39:11 +0100

    Allow setting a custom search string in test_geocoding

--- mainapp/management/commands/test_geocoding.py
+++ mainapp/management/commands/test_geocoding.py
@@ -1,5 +1,4 @@
 import logging
-import math
 
 from django.core.management import BaseCommand, CommandError
 
@@ -9,16 +8,17 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--search-str", default="Marienplatz 1, München, Deutschland"
+        )
+
     def handle(self, *args, **options):
-        search_str = "Marienplatz 1, München, Deutschland"
+        search_str = options["search_str"]
         geometry = geocode(search_str)
         if not geometry:
             raise CommandError("Geocoding failed")
-        elif (
-            geometry["type"] == "Point"
-            and math.isclose(geometry["coordinates"][0], 11.5748977)
-            and math.isclose(geometry["coordinates"][1], 48.1375615)
-        ):
+        elif geometry["type"] == "Point":
             logger.info(f"Success! Geometry for '{search_str}': {geometry}")
         else:
             raise CommandError(
