c681f046ba4f43d90b72db42b4b75b04ecde77fa
konstin <konstin@mailbox.org>
2020-03-23 14:31:41 +0100

    Don't use pytest outsite of dev

--- mainapp/management/commands/test_geocoding.py
+++ mainapp/management/commands/test_geocoding.py
@@ -1,9 +1,10 @@
 import logging
+import math
 
 from django.core.management import BaseCommand, CommandError
 
 from mainapp.functions.geo_functions import geocode
-from pytest import approx
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,7 +14,13 @@ class Command(BaseCommand):
         geometry = geocode(search_str)
         if not geometry:
             raise CommandError("Geocoding failed")
-        elif geometry == {"type": "Point", "coordinates": [approx(11.5748977), approx(48.1375615)]}:
+        elif (
+            geometry["type"] == "Point"
+            and math.isclose(geometry["coordinates"][0], 11.5748977)
+            and math.isclose(geometry["coordinates"][1], 48.1375615)
+        ):
             logger.info(f"Success! Geometry for '{search_str}': {geometry}")
         else:
-            raise CommandError(f"Wrong geometry returned for '{search_str}': {geometry}")
+            raise CommandError(
+                f"Wrong geometry returned for '{search_str}': {geometry}"
+            )
