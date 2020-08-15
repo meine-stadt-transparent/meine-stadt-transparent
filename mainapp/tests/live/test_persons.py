d0f933bdafe54291ede0fa1903bedd86dd26985b
konstin <konstin@mailbox.org>
2020-08-15 20:00:01 +0200

    Fix test_persons with webpack 4

--- mainapp/tests/live/test_persons.py
+++ mainapp/tests/live/test_persons.py
@@ -6,8 +6,13 @@ from mainapp.tests.live.chromedriver_test_case import ChromeDriverTestCase
 class PersonsTest(ChromeDriverTestCase):
     fixtures = ["initdata"]
 
-    def _get_pos_by_name(self, name):
-        js = '$(".persons-list").data("get-item-pos-by-name")("' + name + '");'
+    def _get_pos_by_name(self, name: str):
+        # The position is identified by the tabindex
+        js = (
+            f'Array.from(document.querySelectorAll(".shuffle-item--visible"))'
+            f'.find(x => x.dataset.name == "{name}")?'
+            f'.querySelector("a").tabIndex'
+        )
         pos = self.browser.evaluate_script(js)
         return pos
 
