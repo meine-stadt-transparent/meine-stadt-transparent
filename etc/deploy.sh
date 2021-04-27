f42bf4327d0711344a5fde3cab5e566473da0248
konstin <konstin@mailbox.org>
2021-04-27 19:15:40 +0200

    Remove unused line

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -7,7 +7,6 @@ docker-compose pull
 SHA=$(docker inspect -f '{{ index .Config.Labels "org.opencontainers.image.revision" }}' konstin2/meine-stadt-transparent:main)
 VERSION="meine-stadt-transparent@${SHA}"
 echo $VERSION
-SENTRY_ORG=konstin
 
 sentry-cli releases new -p mst-many "$VERSION"
 sentry-cli releases set-commits "$VERSION" --commit "meine-stadt-transparent/meine-stadt-transparent@${SHA}"
