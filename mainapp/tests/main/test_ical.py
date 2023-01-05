8e39a7977ad3288eb92012d3b0ec4b3261eab7ab
konstin <konstin@mailbox.org>
2023-01-05 17:24:46 +0100

    More python updates

--- mainapp/tests/main/test_ical.py
+++ mainapp/tests/main/test_ical.py
@@ -8,8 +8,8 @@ VERSION:2.0
 PRODID:-//Meine Stadt Transparent//
 BEGIN:VEVENT
 SUMMARY:Meeting with Remy
-DTSTART;TZID=Europe/Berlin;VALUE=DATE-TIME:20170910T120000
-DTEND;TZID=Europe/Berlin;VALUE=DATE-TIME:20170910T180000
+DTSTART;TZID=Europe/Berlin:20170910T120000
+DTEND;TZID=Europe/Berlin:20170910T180000
 UID:meeting-1@meine-stadt-transparent.local
 DESCRIPTION:Meeting with Remy Danton\nhttps://meine-stadt-transparent.loca
  l/meeting/1/
@@ -30,8 +30,8 @@ VERSION:2.0
 PRODID:-//Meine Stadt Transparent//
 BEGIN:VEVENT
 SUMMARY:House Assembly 1
-DTSTART;TZID=Europe/Berlin;VALUE=DATE-TIME:20170901T120000
-DTEND;TZID=Europe/Berlin;VALUE=DATE-TIME:20170901T140000
+DTSTART;TZID=Europe/Berlin:20170901T120000
+DTEND;TZID=Europe/Berlin:20170901T140000
 UID:meeting-3@meine-stadt-transparent.local
 DESCRIPTION:House Assembly Meeting 1\nhttps://meine-stadt-transparent.loca
  l/meeting/3/
@@ -42,8 +42,8 @@ X-ALT-DESC;FMTTYPE=TEXT/HTML:<html>\n<body>\n<p>House Assembly Meeting 1</
 END:VEVENT
 BEGIN:VEVENT
 SUMMARY:House Assembly 2
-DTSTART;TZID=Europe/Berlin;VALUE=DATE-TIME:20170908T120000
-DTEND;TZID=Europe/Berlin;VALUE=DATE-TIME:20170908T140000
+DTSTART;TZID=Europe/Berlin:20170908T120000
+DTEND;TZID=Europe/Berlin:20170908T140000
 UID:meeting-4@meine-stadt-transparent.local
 DESCRIPTION:House Assembly Meeting 2\nhttps://meine-stadt-transparent.loca
  l/meeting/4/
@@ -54,8 +54,8 @@ X-ALT-DESC;FMTTYPE=TEXT/HTML:<html>\n<body>\n<p>House Assembly Meeting 2</
 END:VEVENT
 BEGIN:VEVENT
 SUMMARY:House Assembly 3
-DTSTART;TZID=Europe/Berlin;VALUE=DATE-TIME:20170915T120000
-DTEND;TZID=Europe/Berlin;VALUE=DATE-TIME:20170915T140000
+DTSTART;TZID=Europe/Berlin:20170915T120000
+DTEND;TZID=Europe/Berlin:20170915T140000
 UID:meeting-5@meine-stadt-transparent.local
 DESCRIPTION:House Assembly Meeting 3 - Joint Meeting with Inquiry\nhttps:/
  /meine-stadt-transparent.local/meeting/5/
@@ -67,8 +67,8 @@ X-ALT-DESC;FMTTYPE=TEXT/HTML:<html>\n<body>\n<p>House Assembly Meeting 3 -
 END:VEVENT
 BEGIN:VEVENT
 SUMMARY:House Assembly 4
-DTSTART;TZID=Europe/Berlin;VALUE=DATE-TIME:20170922T120000
-DTEND;TZID=Europe/Berlin;VALUE=DATE-TIME:20170922T140000
+DTSTART;TZID=Europe/Berlin:20170922T120000
+DTEND;TZID=Europe/Berlin:20170922T140000
 UID:meeting-6@meine-stadt-transparent.local
 DESCRIPTION:House Assembly Meeting 4\nhttps://meine-stadt-transparent.loca
  l/meeting/6/
@@ -88,22 +88,20 @@ class TestICal(TestCase):
 
     def test_meeting(self):
         response = self.client.get("/meeting/1/ical/").content.decode().strip()
-        self.assertEqual(response, expected_meeting)
+        assert response == expected_meeting
 
         event = icalendar.cal.Component.from_ical(response).subcomponents[0]
-        self.assertEqual(event.get("dtstart").dt.tzinfo.zone, settings.TIME_ZONE)
-        self.assertEqual(event.get("dtstart").dt.hour, 12)
-        self.assertEqual(event.get("dtend").dt.tzinfo.zone, settings.TIME_ZONE)
-        self.assertEqual(event.get("dtend").dt.hour, 18)
+        assert event.get("dtstart").dt.tzinfo.zone == settings.TIME_ZONE
+        assert event.get("dtstart").dt.hour == 12
+        assert event.get("dtend").dt.tzinfo.zone == settings.TIME_ZONE
+        assert event.get("dtend").dt.hour == 18
 
     def test_meeting_series(self):
         response = self.client.get("/organization/2/ical/").content.decode().strip()
-        self.assertEqual(response, expected_meeting_series)
-        self.assertEqual(
-            len(icalendar.cal.Component.from_ical(response).subcomponents[0]), 7
-        )
+        assert response == expected_meeting_series
+        assert len(icalendar.cal.Component.from_ical(response).subcomponents[0]) == 7
 
     def calendar(self):
         """Just checks that no excpetion is thrown."""
         response = self.client.get("/calendar/ical")
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
